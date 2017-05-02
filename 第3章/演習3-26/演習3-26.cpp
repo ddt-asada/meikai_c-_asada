1f44c75f8e757f3c4994ec443a09d9214d74b224
ddt-asada <ddt.asada@gmail.com>
2017-05-02 15:50:42 +0900

    Revert "明解C++　入門編　演習問題3-26　100％　コミット　（浅田"
    This reverts commit baaeff78b47c24f43b5edeb8d68bab5b4f7b807e.

--- 第3章/演習3-26/演習3-26.cpp
+++ 第3章/演習3-26/演習3-26.cpp
@@ -21,8 +21,6 @@ int main()
 	//繰り返しを判定するための文字列の型。
 	string retry;
 
-	//勝利数をカウントするための型。
-	int w , l = 0;
 	//乱数の種を時間を参照に決定。
 	srand(time(NULL));
 
@@ -39,7 +37,7 @@ int main()
 		//人間のじゃんけんの手を選ぶ。
 		cout << "じゃんけん（グー…0／チョキ…1／パー…2）";
 		cin >> hand;
-		}while(hand < 0 || hand > 2);
+		}while(hand < 0 || hand > 2)
 
 		//人間の手によって3パターンに分岐。
 		switch(hand){
@@ -73,59 +71,5 @@ int main()
 					//パーの時。
 					case 2 : cout <<"あいこ。\n"; break;
 		} break;
-		}
-		//もう一度繰り返すかの確認。
-		cout <<"もう一度？（Yes…Y／No…N）";
-		cin >> retry;
-		//じゃんけんを繰り返すか判定。
-	}while(retry == "y" || retry == "Y");
-
-//いずれか一方が3回勝つまで繰り返すじゃんけん。
-	do{
-	//コンピュータのじゃんけんの手を判定するための乱数。
-	int cpu = rand()%3;
-
-	//三つのうちのいずれかが入力されるまで繰り返す。
-	do{
-	//人間のじゃんけんの手を選ぶ。
-	cout << "じゃんけん（グー…0／チョキ…1／パー…2）";
-	cin >> hand;
-	}while(hand < 0 || hand > 2);
-
-	//人間の手によって3パターンに分岐。
-	switch(hand){
-	//グーの時。
-	case 0 : switch(cpu){
-	//コンピュータの手によって3パターンに分岐。
-				//グーの時。
-				case 0 : cout <<"あいこ。\n"; break;
-				//チョキの時。
-				case 1 : cout <<"勝ち。\n"; w++; break;
-				//パーの時。
-				case 2 : cout <<"負け。\n"; l++; break;
-	} break;
-	//チョキの時。
-	case 1 : switch(cpu){
-	//コンピュータの手によって3パターンに分岐。
-				//グーの時。
-				case 0 : cout <<"負け。\n"; l++; break;
-				//チョキの時。
-				case 1 : cout <<"あいこ。\n"; break;
-				//パーの時。
-				case 2 : cout <<"勝ち。\n"; w++; break;
-	} break;
-	//パーの時。
-	case 2 : switch(cpu){
-	//コンピュータの手によって3パターンに分岐。
-				//グーの時。
-				case 0 : cout <<"勝ち。\n"; w++; break;
-				//チョキの時。
-				case 1 : cout <<"負け。\n"; l++; break;
-				//パーの時。
-				case 2 : cout <<"あいこ。\n"; break;
-	} break;
 	}
-	//じゃんけんを繰り返すか判定。
-}while(w < 3 || l < 3);
-
 }
