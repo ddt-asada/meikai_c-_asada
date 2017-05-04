eea262189c1b623cbde22702dc34b3b6f6834af5
ddt-asada <ddt.asada@gmail.com>
2017-05-04 11:44:55 +0900

    C++　演習2-15　修正100％　2回目コミット　（浅田　コメントを追加・修正。　ソースコード全体を見やすく整頓。

--- 第2章/演習2-15/演習2-15.cpp
+++ 第2章/演習2-15/演習2-15.cpp
@@ -2,6 +2,8 @@
  * 乱数に応じて「大吉」、「中吉」、「小吉」、「吉」、「末吉」、「凶」、「大凶」のいずれかを表示するプログラム。
  * 作成日：2017年5月1日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月4日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
@@ -17,24 +19,23 @@ int main()
 	//乱数の種を時間を参照にする。
 	srand(time(NULL));
 
-	//0～6の範囲（7種類の値）の乱数を生成する。
-	int a = rand() % 7;
+	int hand = rand() % 7;	//0～6の範囲の乱数を生成し、じゃんけんの手の判定用の型に代入。
 
 	//乱数の値に応じて結果を表示。
-	switch(a){
+	switch(hand) {
 	//0の時、末尾にswitch文から抜けるためのbreakを配置。
-	case 0 : cout <<"大吉\n";	break;
+	case 0		:	cout	<< "大吉\n";	break;
 	//1の時、末尾にswitch文から抜けるためのbreakを配置。
-	case 1 : cout <<"中吉\n";	break;
+	case 1		:	cout	<< "中吉\n";	break;
 	//2の時、末尾にswitch文から抜けるためのbreakを配置。
-	case 2 : cout <<"吉\n";		break;
+	case 2		:	cout	<< "吉\n";		break;
 	//3の時、末尾にswitch文から抜けるためのbreakを配置。
-	case 3 : cout <<"末吉\n";	break;
+	case 3		:	cout	<< "末吉\n";	break;
 	//4の時、末尾にswitch文から抜けるためのbreakを配置。
-	case 4 : cout <<"凶\n";		break;
+	case 4		:	cout	<< "凶\n";		break;
 	//5の時、末尾にswitch文から抜けるためのbreakを配置。
-	case 5 : cout <<"大凶\n";	break;
+	case 5		:	cout	<< "大凶\n";	break;
 	//上記に当てはまらない時、switch文最後のため、breakは配置しない。
-	default : cout <<"小吉\n";
+	default	:	cout	<< "小吉\n";
 	}
 }
