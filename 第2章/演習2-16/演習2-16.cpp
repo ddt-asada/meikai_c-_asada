35f572cc7303ee1a0ad98fba88f6887941c663d2
ddt-asada <ddt.asada@gmail.com>
2017-05-04 11:50:35 +0900

    C++　演習2-16　修正100％　2回目コミット　（浅田　コメントを追加・修正。　ソースコードを全体を見やすく整頓。

--- 第2章/演習2-16/演習2-16.cpp
+++ 第2章/演習2-16/演習2-16.cpp
@@ -1,7 +1,9 @@
 /*演習2-16
- * 1～12の整数を読み込んで、それに対応する季節をひょうじするプログラム。
+ * 1～12の整数を読み込んで、それに対応する季節を表示するプログラム。
  * 作成日：2017年5月1日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月4日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
@@ -10,26 +12,26 @@ using namespace std;
 
 int main()
 {
-	//入力するための変数を生成。
-	int month ;
+	int month ;		//月を入力するための型。
 
-	//入力を促す。
-	cout <<"何月ですか？：";
-	cin >>month;
+	//月の入力を促す。
+	cout	<< "何月ですか？：";
+	cin		>> month;
 
-	switch(month){
+	switch(month) {
 	//monthが3～5の時の分岐、switch文は下に落ちていく性質を利用し、breakを途中に配置し、簡略化。
-	case 3 :;
-	case 4 :;
-	case 5 : cout <<"それは春です。\n";	break;
+	case 3		:	;
+	case 4		:	;
+	case 5		:	cout << "それは春です。\n";	break;
 	//monthが6～8の時の分岐、switch文は下に落ちていく性質を利用し、breakを途中に配置し、簡略化。
-	case 6 :;
-	case 7 :;
-	case 8 : cout <<"それは夏です。\n";	break;
+	case 6		:	;
+	case 7 		:	;
+	case 8		: 	cout << "それは夏です。\n";	break;
 	//monthが9～11の時の分岐、switch文は下に落ちていく性質を利用し、breakを途中に配置し、簡略化。
-	case 9 :;
-	case 10:;
-	case 11: cout <<"それは秋です。\n";	break;
-	default 		  : cout <<"それは冬です。\n";
+	case 9		:	;
+	case 10		:	;
+	case 11		:	cout << "それは秋です。\n";	break;
+	//それにも当てはまらない場合。
+	default 	:	cout << "それは冬です。\n";
 	}
 }
