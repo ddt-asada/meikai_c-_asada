fe8ea3234c9fc655b726eb85e6ef56e05467f722
ddt-asada <ddt.asada@gmail.com>
2017-05-05 18:20:16 +0900

    C++ 演習4-9 修正100％ （浅田 変数名を変更。 ソースコード全体を見やすく整頓。 コメントを追加・修正。

--- e_04_09/src/e_04_09.cpp
+++ e_04_09/src/e_04_09.cpp
@@ -2,6 +2,8 @@
  * 三つの整数値を読み込んで合計と平均を表示するプログラム。
  * 作成日：2017年5月2日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月5日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
@@ -10,26 +12,41 @@ using namespace std;
 
 int main()
 {
-	//計算に使用する三つの整数値の型を生成。
-	int a,b,c;
+	int	Cal1, Cal2, Cal3;	//計算に使用する三つの整数値の型を生成。
 
-	//計算したい整数値の入力を促す。
-	cout <<"整数a：";
-	cin >>a;
-	cout <<"整数b：";
-	cin >>b;
-	cout <<"整数c：";
-	cin >>c;
+	//「整数Cal1：」と表示。
+	cout	<<"整数Cal1：";
+	//計算に使用する整数値の入力。
+	cin		>>Cal1;
 
-	//キャスト記法により型の変換を行う。
-	cout <<"合計は"<<(double)(a + b + c)<<"です。\n";
-	cout <<"平均は"<<(double)(a + b + c)/3<<"です。\n";
+	//「整数Cal2：」と表示。
+	cout	<<"整数Cal2：";
+	//計算に使用する整数値の入力。
+	cin		>>Cal2;
+
+	//「整数Cal3：」と表示。
+	cout	<<"整数Cal3：";
+	//計算に使用する整数値の入力。
+	cin		>>Cal3;
+
+	//キャスト記法により型の変換を行う
+	//合計をdouble型に変換して表示。
+	cout	<<"合計は"	<<(double)(Cal1 + Cal2 + Cal3)		<<"です。\n";
+	//平均をdouble型に変換して表示。
+	cout	<<"平均は"	<<(double)(Cal1 + Cal2 + Cal3) / 3	<<"です。\n";
 
 	//関数的記法により型の変換を行う。
-	cout <<"合計は"<<double(a + b + c)<<"です。\n";
-	cout <<"平均は"<<double(a + b + c)/3<<"です。\n";
+	//合計をdouble型に変換して表示。
+	cout	<<"合計は"	<<double(Cal1 + Cal2 + Cal3)		<<"です。\n";
+	//平均をdouble型に変換して表示。
+	cout	<<"平均は"	<<double(Cal1 + Cal2 + Cal3) / 3	<<"です。\n";
 
 	//staticcast演算子を用いて型の変換を行う。
-	cout <<"合計は"<<static_cast<double>(a + b + c)<<"です。\n";
-	cout <<"平均は"<<static_cast<double>(a + b + c)/3<<"です。\n";
+	//合計をdouble型に変換して表示。
+	cout	<<"合計は"	<<static_cast<double> (Cal1 + Cal2 + Cal3)		<<"です。\n";
+	//平均をdouble型に変換して表示。
+	cout	<<"平均は"	<<static_cast<double> (Cal1 + Cal2 + Cal3) / 3	<<"です。\n";
+
+	//0を返す。
+	return 0;
 }
