3614ece523e9f199ccb90d46d104e49be737a829
ddt-asada <ddt.asada@gmail.com>
2017-05-05 18:00:00 +0900

    C++ 演習4-7 修正100％ （浅田 変数名を変更。 ソースコード全体を見やすく整頓。 コメントの追加・修正

--- e_04_07/src/e_04_07.cpp
+++ e_04_07/src/e_04_07.cpp
@@ -2,6 +2,8 @@
  * 3種類の浮動小数点型の情報をtypeid演算子によって表示するプログラム。
  * 作成日：2017年5月2日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月5日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
@@ -13,12 +15,22 @@ using namespace std;
 int main()
 {
 	//3種類の浮動小数点型を生成する。
-	float a =1.23456;
-	double b =1.23456;
-	long double c =1.23456;
+	float			fTest =1.23456;		//float型に1.23456を代入。
 
-	//それぞれの型情報を表示。
-	cout <<"変数aの型："<<typeid(a).name()<<'\n';
-	cout <<"変数bの型："<<typeid(b).name()<<'\n';
-	cout <<"変数cの型："<<typeid(c).name()<<'\n';
+	double			dTest =1.23456;		//double型に1.23456を代入。
+
+	long double 	ldTest =1.23456;	//long_double型に1.23456を代入。
+
+	//それぞれの型を表示。
+	//floatの型を表示。
+	cout	<<"変数fTestの型："	<<typeid(fTest).name()	<<'\n';
+
+	//doubleの型を表示。
+	cout	<<"変数dtestの型："	<<typeid(dTest).name()	<<'\n';
+
+	//long_doubleの型を表示。
+	cout	<<"変数ldTestの型："<<typeid(ldTest).name()<<'\n';
+
+	//0を返す。
+	return 0;
 }
