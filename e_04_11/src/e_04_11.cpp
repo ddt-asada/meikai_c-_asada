deb0a0f44ba981b2e1c75e1e7921cba1ba13398e
ddt-asada <ddt.asada@gmail.com>
2017-05-05 18:32:11 +0900

    C++ 演習4-11 修正100％ （浅田 変数名を変更。 ソースコード全体を見やすく整頓。 コメントを追加・修正。

--- e_04_11/src/e_04_11.cpp
+++ e_04_11/src/e_04_11.cpp
@@ -2,6 +2,8 @@
  * float型の変数を0.0から1.0まで0.001ずつ増やしながら、その値と、その値の2乗を表示するプログラムを作成せよ。
  * 作成日：2017年5月2日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月5日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
@@ -13,20 +15,26 @@ using namespace std;
 int main()
 {
 	//表を見やすくするため、setw操作子により10文字幅を確保。
-	cout << setw(10) << "float";
-	cout << setw(10) << "2乗値\n";
+	//10文字の間隔に「folat」と表示。
+	cout	<<setw(10)	<<"float";
+
+	//10文字の間隔に「2乗値」と表示。
+	cout	<<setw(10)	<<"2乗値\n";
+
 	//表題との境目。
-	cout << "-----------------\n";
+	cout	<<"-----------------\n";
 
 	//浮動小数点の精度を指定。
-	cout << fixed <<setprecision(6);
+	cout	<<fixed	<<setprecision(6);
 
 	//0.001～1.000までの繰り返しを宣言。
-	for(float x = 0.0F; x <= 1.0F; x +=0.001F){
+	for(float fPow = 0.0F; fPow <= 1.0F; fPow += 0.001F) {
 		//見やすくするため10文字幅を確保。
-		cout << setw(10) << x;
+		cout	<<setw(10)	<<fPow;
 		//見やすくするため10文字幅を確保。
-		cout << setw(10) << x*x<<'\n';
+		cout	<<setw(10)	<<fPow * fPow	<<'\n';
 	}
 
+	//0を返す。
+	return 0;
 }
