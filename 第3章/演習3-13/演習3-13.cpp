7ede13325d49e486e366b0598fd18264aaa494d2
ddt-asada <ddt.asada@gmail.com>
2017-05-05 09:53:20 +0900

    C++　演習3-13　修正100％　2回目コミット　（浅田　変数名を変更。　ソースコード全体を見やすく整頓。　コメントを追加・修正。

--- 第3章/演習3-13/演習3-13.cpp
+++ 第3章/演習3-13/演習3-13.cpp
@@ -2,6 +2,8 @@
  * 身長と標準体重の対応表を表示するプログラム。
  * 作成日：2017年5月1日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月5日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
@@ -10,25 +12,34 @@ using namespace std;
 
 int main()
 {
-	//入力するための変数を生成。
-	int a,b,c;
+	int	Range1, Range2;		//求めたい身長の範囲の型。
 
-	//求めたい身長の範囲の入力を促す。
-	cout <<"何cmから：";
-	cin >>a;
+	int	Interval;			//求める身長の間隔の型。
 
-	cout <<"何cmまで：";
-	cin >>b;
+	//「何cmから：」と表示。
+	cout	<<"何cmから：";
+	//求めたい身長の下限の入力。
+	cin		>>Range1;
 
-	//求めたい身長の間隔の入力を促す。
-	cout <<"何cmごとに：";
-	cin >> c;
+	//「何cmまで：」と表示。
+	cout	<<"何cmまで：";
+	//求めたい身長の上限の入力。
+	cin		>>Range2;
 
+	//「何cmごとに：」と表示。。
+	cout	<<"何cmごとに：";
+	//求めたい身長の間隔の入力。
+	cin		>>Interval;
+
+	//「身長	標準体重」と表示。
 	cout <<"身長	標準体重\n";
 
-	//acmからbcmの範囲の標準体重をccmごとに表示するためのfor文
-	for(int i = a; i <= b; i += c)
-		//標準体重を出力。
-		cout <<i<<"		"<<(i-100)*0.9<<'\n';
+	//Range1cmからRange2cmの範囲の標準体重をIntervalcmごとに表示するためのfor文
+	for(int i = Range1; i <= Range2; i += Interval) {
+		//身長とそれに対応した標準体重を出力。
+		cout	<<i	<<"		"	<<(i - 100) * 0.9	<<'\n';
+	}
 
+	//0を返す。
+	return 0;
 }
