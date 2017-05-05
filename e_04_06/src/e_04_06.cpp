2af613e6e5930f9a0245f843de81da548b4fccff
ddt-asada <ddt.asada@gmail.com>
2017-05-05 17:54:17 +0900

    C++ 演習4-6 修正100％ （浅田 変数名を変更。 ソースコード全体を見やすく整頓。 コメントの追加・修正。

--- e_04_06/src/e_04_06.cpp
+++ e_04_06/src/e_04_06.cpp
@@ -2,6 +2,8 @@
  * 3種類の浮動小数点型の大きさをsizeof演算子によって表示するプログラム。
  * 作成日：2017年5月2日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月5日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
@@ -11,12 +13,22 @@ using namespace std;
 int main()
 {
 	//3種類の浮動小数点型を生成する。
-	float a =1.23456;
-	double b =1.23456;
-	long double c =1.23456;
+	float			fTest =1.23456;		//float型に1.23456を代入。
+
+	double			dTest =1.23456;		//double型に1.23456を代入。
+
+	long double 	ldTest =1.23456;	//long_double型に1.23456を代入。
 
 	//それぞれの大きさを表示。
-	cout <<"変数aの大きさ："<<sizeof (a)<<'\n';
-	cout <<"変数bの大きさ："<<sizeof (b)<<'\n';
-	cout <<"変数cの大きさ："<<sizeof (c)<<'\n';
+	//floatの大きさを表示。
+	cout	<<"変数fTestの大きさ："	<<sizeof(fTest)	<<'\n';
+
+	//doubleの大きさを表示。
+	cout	<<"変数dtestの大きさ："	<<sizeof(dTest)	<<'\n';
+
+	//long_doubleの大きさを表示。
+	cout	<<"変数ldTestの大きさ："<<sizeof(ldTest)	<<'\n';
+
+	//0を返す。
+	return 0;
 }
