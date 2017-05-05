9a9c45cfe494056527f685aa2918963f256a9d1c
ddt-asada <ddt.asada@gmail.com>
2017-05-05 18:49:46 +0900

    C++ 演習4-13 修正100％ （浅田 ソースコード全体を見やすく整頓。 コメントを追加・修正。 do文をfor文に変更。

--- e_04_13/src/e_04_13.cpp
+++ e_04_13/src/e_04_13.cpp
@@ -1,7 +1,9 @@
 /*演習4-13
- * 曜日を表す列挙隊を定義し、プログラムを作成。
+ * 曜日を表す列挙体を定義し、プログラムを作成。
  * 作成日：2017年5月2日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月5日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
@@ -10,33 +12,42 @@ using namespace std;
 
 int main()
 {
-	//曜日を表す列挙体を宣言。分かりやすくするためにあえてMonを1で初期化。
-	enum Day{Mon=1, Tues, Wed, Thu, Fri, Sat, Sun, Invaild};
+	enum	Day{Mon = 1, Tues, Wed, Thu, Fri, Sat, Sun, Invaild};	//曜日を表す列挙体を宣言。分かりやすくするためにあえてMonを1で初期化。
 
-	//曜日を選択するための変数の型。
-	int type;
+	int 	type = 0;												//曜日を選択するための変数を0で初期化（繰り返しの判定で利用するため）。
 
 	//曜日の範囲以外の変数が入力されたときは再入力させる。
-	do{
-		cout << "1…月曜日　2…火曜日　3…水曜日　4…木曜日　5…金曜日　6…土曜日　7…金曜日　8…終了：";
-		cin >> type;
-	}while(type < Mon || type > Invaild);
+	for( ; type < Mon || type > Invaild; ) {
+		//「"1…月曜日　2…火曜日　3…水曜日　4…木曜日　5…金曜日　6…土曜日　7…金曜日　8…終了：」と表示。
+		cout	<<"1…月曜日　2…火曜日　3…水曜日　4…木曜日　5…金曜日　6…土曜日　7…金曜日　8…終了：";
+		//曜日の入力を促す。
+		cin		>>type;
+	}
 
 	//終了が選択されたとき以外の分岐。
-	if(type != Invaild);
-
+	if(type != Invaild) {
 	//tint型変数をDay型変数に変換。
 	Day selected = static_cast<Day>(type);
 
-	//選択した曜日により分岐。
-	switch(selected){
-	case Mon	: cout <<"今日は月曜日です。\n";	break;
-	case Tues	: cout <<"今日は火曜日です。\n";	break;
-	case Wed	: cout <<"今日は水曜日です。\n";	break;
-	case Thu	: cout <<"今日は木曜日です。\n";	break;
-	case Fri	: cout <<"今日は金曜日です。\n";	break;
-	case Sat	: cout <<"今日は土曜日です。\n";	break;
-	case Sun	: cout <<"今日は日曜日です。\n";	break;
+		//選択した曜日により分岐。
+		switch(selected) {
+			//月曜日の時の分岐。
+			case Mon	:cout	<<"今日は月曜日です。\n";	break;
+			//火曜日の時の分岐。
+			case Tues	:cout	<<"今日は火曜日です。\n";	break;
+			//水曜日の時の分岐。
+			case Wed	:cout	<<"今日は水曜日です。\n";	break;
+			//木曜日の時の分岐。
+			case Thu	:cout	<<"今日は木曜日です。\n";	break;
+			//金曜日の時の分岐。
+			case Fri	:cout	<<"今日は金曜日です。\n";	break;
+			//土曜日の時の分岐。
+			case Sat	:cout	<<"今日は土曜日です。\n";	break;
+			//日曜日の時の分岐。
+			case Sun	:cout	<<"今日は日曜日です。\n";	break;
+		}
 	}
 
+	//0を返す。
+	return 0;
 }
