534c70a00045c970a66073c9a45fe7e3ed8d88cb
ddt-asada <ddt.asada@gmail.com>
2017-05-11 10:30:56 +0900

    C++ 演習6-8 修正完了100％ （浅田 テキストの課題に沿うように、関数宣言により関数の定義順序を変更。

--- e_06_08/src/e_06_08.cpp
+++ e_06_08/src/e_06_08.cpp
@@ -4,52 +4,19 @@
  * 作成者：浅田　知嗣
  * 更新日：2017年5月7日
  * 更新者：浅田　知嗣
+ * 更新日：2017年5月10日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
 
 using namespace std;
 
-/*関数名：void put_nchar(int Num, char Letter)
- * 受け取った引数Numが0になるまで受け取った文字Letterを表示する関数。
- * 引数Num：繰り返す回数。
- * 引数Letter：表示する文字。
- * 返却値：なし。
- * 作成日：2017年5月3日
- * 作成者：浅田　知嗣
- * 更新日：2017年5月7日
- * 更新者：浅田　知嗣
- */
+//関数宣言。
+void put_nchar(int Num, char Letter);
 
-//引数Numが0になるまで文字Letterを表示する関数。
-void put_nchar(int Num, char Letter)
-{
-	//Numが0になるまで文字列を出力。
-	for(int i = Num; i > 0; i--) {
-
-		//文字Letterを表示。
-		cout	<<Letter;
-
-	}
-}
-
-/*関数名：void put_stars(int Num)
- * 受け取った引数を引数が0になるまで文字Letterを表示する関数に渡す関数。
- * 引数Num：繰り返す回数。
- * 返却値：なし。
- * 作成日：2017年5月3日
- * 作成者：浅田　知嗣
- * 更新日：2017年5月7日
- * 更新者：浅田　知嗣
- */
-
-//引数Numが0になるまで文字cを表示する関数を呼び出す関数。
-void put_stars(int Num)
-{
-	//関数に繰り返したい回数、表示したい文字を渡す。
-	put_nchar(Num, '*');
-
-}
+//関数宣言。
+void put_stars(int Num);
 
 int main()
 {
@@ -73,4 +40,49 @@ int main()
 
 	//0を返す。
 	return 0;
+}
+
+/*関数名：void put_nchar(int Num, char Letter)
+ * 受け取った引数Numが0になるまで受け取った文字Letterを表示する関数。
+ * 引数Num：繰り返す回数。
+ * 引数Letter：表示する文字。
+ * 返却値：なし。
+ * 作成日：2017年5月3日
+ * 作成者：浅田　知嗣
+ * 更新日：2017年5月7日
+ * 更新者：浅田　知嗣
+ * 更新日：2017年5月10日
+ * 更新者：浅田　知嗣
+ */
+
+//引数Numが0になるまで文字Letterを表示する関数。
+void put_nchar(int Num, char Letter)
+{
+	//Numが0になるまで文字列を出力。
+	for(int i = Num; i > 0; i--) {
+
+		//文字Letterを表示。
+		cout	<<Letter;
+
+	}
+}
+
+/*関数名：void put_stars(int Num)
+ * 受け取った引数を引数が0になるまで文字Letterを表示する関数に渡す関数。
+ * 引数Num：繰り返す回数。
+ * 返却値：なし。
+ * 作成日：2017年5月3日
+ * 作成者：浅田　知嗣
+ * 更新日：2017年5月7日
+ * 更新者：浅田　知嗣
+ * 更新日：2017年5月10日
+ * 更新者：浅田　知嗣
+ */
+
+//引数Numが0になるまで文字cを表示する関数を呼び出す関数。
+void put_stars(int Num)
+{
+	//関数に繰り返したい回数、表示したい文字を渡す。
+	put_nchar(Num, '*');
+
 }
