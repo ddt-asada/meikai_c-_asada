7d97296b93b54006a158a22abd7cda9a83d0da62
ddt-asada <ddt.asada@gmail.com>
2017-05-12 11:45:37 +0900

    C++ 演習8-9 修正100％ （浅田 エラーが出ていたのでエラーが出ないように変更。 void関数の中身が未完成だったので完成させた。 平澤さんのチェックはまだ。

--- e_08_09_02/src/e_08_09_02.cpp
+++ e_08_09_02/src/e_08_09_02.cpp
@@ -4,6 +4,8 @@
  * 作成者：浅田　知嗣
  * 更新日：2017年5月11日
  * 更新者：浅田　知嗣
+ * 更新日：2017年5月12日
+ * 更新者：浅田　知嗣
  */
 #include <iostream>
 //文字列処理関数を提供するヘッダ。
@@ -16,14 +18,33 @@ void mystrncpy(char* paste, const char* copy, int range);
 
 int main()
 {
-	const char*	copy = "abcdef";		//コピー元のポインタによる文字列を生成。
+	//コピー元の文字数がnより長いパターンの確認。
+	const char	copy1[] = "abcde";		//コピー元のポインタによる文字列を生成（5文字）。
 
-	char*		paste = "ghijklmnopqr";	//コピー先のポインタによる文字列を生成。
+	char		paste1[] = "123456789";	//コピー先のポインタによる文字列を生成（9文字）。
 
-	int			range = 5;			//何文字までコピーするかの範囲を設定。
+	int			range1 = 3;			//何文字までコピーするかの範囲を設定（3文字）。
 
 	//文字列をコピーする関数を呼び出す。
-	mystrncpy(paste, copy, range);
+	mystrncpy(paste1, copy1, range1);
+
+	//コピーした文字列の表示。
+	cout	<<"コピーが終わりました。コピー後の文字列は\n"	<<paste1	<<"です。\n";
+
+
+	//コピー元の文字数がnより短いパターンの確認。
+	const char	copy2[] = "abcde";		//コピー元のポインタによる文字列を生成（5文字）。
+
+	char		paste2[] = "123456789";	//コピー先のポインタによる文字列を生成（9文字）。
+
+	int			range2 = 7;			//何文字までコピーするかの範囲を設定（7文字）。
+
+	//文字列をコピーする関数を呼び出す。
+	mystrncpy(paste2, copy2, range2);
+
+	//コピーした文字列の表示。
+	cout	<<"コピーが終わりました。コピー後の文字列は\n"	<<paste2	<<"です。\n";
+
 
 	//0を返す。
 	return 0;
@@ -39,27 +60,27 @@ int main()
  * 作成者：浅田　知嗣
  * 更新日：2017年5月11日
  * 更新者：浅田　知嗣
+ * 更新日：2017年5月12日
+ * 更新者：浅田　知嗣
  */
 
 void mystrncpy(char* paste, const char* copy,int range)
 {
 
-	int a =strlen(copy);
+	int copyLen = strlen(copy);		//コピー元の文字列の長さを求める。コピー範囲との比較を行いたいため。
+
 	//コピーするための繰り返し。
 	for(int i = 0; i < range; i++) {
 		//コピーの範囲が文字列の長さ未満の時。
-		if(a >= range) {
+		if(copyLen >= i) {
 
 			paste[i] = copy[i];	//文字列をコピーしていく。
 
 		//コピーの範囲が文字列の長さを超えた場合。
-		} else {
+		} else if(copyLen < i) {
 
 			paste[i] = '\0';	//ナル文字で埋め尽くす。
 
 		}
 	}
-
-	//コピーした文字列の表示。
-	cout	<<"コピーが終わりました。コピー後の文字列は"	<<copy	<<'\n'	<<paste	<<'\n';
 }
