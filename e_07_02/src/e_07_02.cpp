715230267b60a54f43f389637d61d75576958f9d
ddt-asada <ddt.asada@gmail.com>
2017-05-12 13:48:18 +0900

    C++ 演習7-2 修正完了100％ （浅田 テキストの課題に沿うように関数宣言を追加。

--- e_07_02/src/e_07_02.cpp
+++ e_07_02/src/e_07_02.cpp
@@ -2,28 +2,16 @@
  * 二つの引数を交換する関数swapを参照渡しではなく、ポインタの値渡しによって行うプログラム。
  * 作成日：2017年5月8日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月10日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
 
 using namespace std;
 
-/*関数名：void swap(int* swap1, int* swap2)
- * 渡された引数を入れ替える関数。
- * 引数swap1：入れ替える値。
- * 引数swap2：入れ替える値。
- * 返却値：なし。
- * 作成日：2017年5月8日
- * 作成者：浅田　知嗣
- */
-
-void swap(int* swap1, int* swap2)
-{
-	int	swap3  = *swap1;	//値を入れ替えるための仮の置き場に、swap1を代入。
-		*swap1 = *swap2;	//値を入れ替える。
-		*swap2 = swap3;	//値を入れ替える。
-
-}
+//関数宣言。
+void swap(int* swap1, int* swap2);
 
 int main()
 {
@@ -50,4 +38,23 @@ int main()
 
 	//0を返す。
 	return 0;
+}
+
+/*関数名：void swap(int* swap1, int* swap2)
+ * 渡された引数を入れ替える関数。
+ * 引数swap1：入れ替える値。
+ * 引数swap2：入れ替える値。
+ * 返却値：なし。
+ * 作成日：2017年5月8日
+ * 作成者：浅田　知嗣
+ * 更新日：2017年5月10日
+ * 更新者：浅田　知嗣
+ */
+
+void swap(int* swap1, int* swap2)
+{
+	int	swap3  = *swap1;	//値を入れ替えるための仮の置き場に、swap1を代入。
+		*swap1 = *swap2;	//値を入れ替える。
+		*swap2 = swap3;	//値を入れ替える。
+
 }
