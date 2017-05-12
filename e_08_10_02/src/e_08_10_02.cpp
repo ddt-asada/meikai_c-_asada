f0a4567fe29f208954bcb2576108be5eee8dd722
ddt-asada <ddt.asada@gmail.com>
2017-05-12 13:56:20 +0900

    C++ 演習8-10 修正100％ （浅田 未完成だったのを完成させた。 平澤さんのチェックはまだです。

--- e_08_10_02/src/e_08_10_02.cpp
+++ e_08_10_02/src/e_08_10_02.cpp
@@ -2,6 +2,8 @@
  * strncat関数の作成。
  * 作成日：2017年5月11日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月12日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
@@ -15,14 +17,25 @@ char* mystrncat(char* cat1, const char* cat2,int range);
 
 int main()
 {
-	const char*	cat2 = "abcdefg";	//末尾に連結する文字列。
+	//末尾に連結する文字列が連結したい長さより長い時の動作を確認。
+	const char	cat2[] = "abcdefg";		//末尾に連結する文字列（7文字）。
 
-	char*		cat1 = "12345";		//先頭に連結する文字列。
+	char		cat1[] = "123";		//先頭に連結する文字列（3文字）。
 
-	int			range = 5;			//末尾に連結する文字列の長さを指定。
+	int			range1 = 5;			//末尾に連結する文字列の長さを指定（5文字）。
 
 	//文字列を連結して表示。
-	cout	<<"文字列を連結しました。連結後の文字列は\n"<<mystrncat(cat1, cat2, range)	<<"です。";
+	cout	<<"文字列を連結しました。連結後の文字列は\n"<<mystrncat(cat1, cat2, range1)	<<"です。\n";
+
+	//末尾に連結する文字列が連結したい長さよりとき短いときの動作を確認。。
+	const char	cat4[] = "abc";		//末尾に連結する文字列（3文字）。
+
+	char		cat3[] = "123";		//先頭に連結する文字列（3文字）。
+
+	int			range2 = 5;			//末尾に連結する文字列の長さを指定（5文字）。
+
+	//文字列を連結して表示。
+	cout	<<"文字列を連結しました。連結後の文字列は\n"<<mystrncat(cat3, cat4, range2)	<<"です。\n";
 
 	//0を返す。
 	return 0;
@@ -36,6 +49,8 @@ int main()
  * 返却値：連結後の文字列cat1。
  * 作成日：2017年5月11日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月12日
+ * 更新者：浅田　知嗣
  */
 
 char* mystrncat(char* cat1, const char* cat2, int range)
@@ -50,14 +65,14 @@ char* mystrncat(char* cat1, const char* cat2, int range)
 		for(int i = 0; i <= range + 1; i++) {
 
 		//処理が最後の時。
-			if(i == lenCat1 + range + 1) {
+			if(i == range + 1) {
 
-				cat1[i + lenCat1] += '\0';		//末尾にナル文字を連結。
+				cat1[i + lenCat1] = '\0';		//末尾にナル文字を連結。
 
 			//それ以外の時。
 			} else {
 
-				cat1[i + lenCat1] += cat2[i];	//末尾に連結していく。
+				cat1[i + lenCat1] = cat2[i];	//末尾に連結していく。
 
 			}
 		}
@@ -69,12 +84,12 @@ char* mystrncat(char* cat1, const char* cat2, int range)
 			//処理が最後の時。
 			if(i == lenCat2) {
 
-				cat1[i + lenCat1] += '\0';		//末尾にナル文字を連結。
+				cat1[i + lenCat1] = '\0';		//末尾にナル文字を連結。
 
 			//それ以外の時。
 			} else {
 
-				cat1[i + lenCat1] += cat2[i];	//末尾に連結していく。
+				cat1[i + lenCat1] = cat2[i];	//末尾に連結していく。
 
 			}
 		}
