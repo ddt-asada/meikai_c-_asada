1a9c57fbab0b42d257cf3482daf08b38bc4652b6
ddt-asada <ddt.asada@gmail.com>
2017-05-12 13:50:32 +0900

    C++ 演習7-8 修正完了100％ （浅田 テキストの課題に沿うように関数宣言を追加。

--- e_07_08/src/e_07_08.cpp
+++ e_07_08/src/e_07_08.cpp
@@ -2,41 +2,16 @@
  * 要素数noの配列arrayから要素array[rmv]を先頭とするmany個の要素を削除する関数を作成。
  * 作成日：2017年5月8日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月10日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
 
 using namespace std;
 
-/*関数名：void aryrmv(int array[], int no, int rmv, int many)
- * 受け取った配列array[no]のうちarray[rmv]を先頭とするmany個の要素を削除し、前方に数字を詰める関数。
- * 引数array[]：削除を行うための配列。
- * 引数no：配列の要素数。
- * 引数rmv：削除したい配列の要素。
- * 引数many：何個削除するか。
- * 返却値：なし。
- * 作成日：2017年5月8日
- * 作成者：浅田　知嗣
- */
-
-void aryrmv(int array[], int no, int rmv, int many)
-{
-	//任意の要素を先頭とするmanyこの要素を削除して前に詰める。
-	for(int i = rmv; i < no; i++) {
-
-		//処理する要素が末尾要素に達した時（前方にずらすべき要素が存在していない）。
-		if(i + many > no - 1) {
-
-			array[i] = array[no - 1];	//末尾の要素を代入。
-
-		//後ろに要素がある場合。
-		} else {
-
-			array[i] = array[i + many];	//任意の要素をi + many個後ろの要素で上書き（数字を前に詰める）。
-
-		}
-	}
-}
+//関数宣言。
+void aryrmv(int array[], int no, int rmv, int many);
 
 int main()
 {
@@ -69,7 +44,7 @@ int main()
 	cin		>>many;
 
 	//任意の要素を削除する関数を呼び出す。
-	aryrmv(array, no, rmv-1, many);
+	aryrmv(array, no, rmv, many);
 
 	//削除された後の配列を表示。
 	cout	<<rmv	<<"任意の要素を削除しました。\n";
@@ -84,4 +59,36 @@ int main()
 
 	//0を返す。
 	return 0;
+}
+
+/*関数名：void aryrmv(int array[], int no, int rmv, int many)
+ * 受け取った配列array[no]のうちarray[rmv]を先頭とするmany個の要素を削除し、前方に数字を詰める関数。
+ * 引数array[]：削除を行うための配列。
+ * 引数no：配列の要素数。
+ * 引数rmv：削除したい配列の要素。
+ * 引数many：何個削除するか。
+ * 返却値：なし。
+ * 作成日：2017年5月8日
+ * 作成者：浅田　知嗣
+ * 更新日：2017年5月10日
+ * 更新者：浅田　知嗣
+ */
+
+void aryrmv(int array[], int no, int rmv, int many)
+{
+	//任意の要素を先頭とするmanyこの要素を削除して前に詰める。
+	for(int i = rmv; i < no; i++) {
+
+		//処理する要素が末尾要素に達した時（前方にずらすべき要素が存在していない）。
+		if(i + many > no - 1) {
+
+			array[i] = array[no - 1];	//末尾の要素を代入。
+
+		//後ろに要素がある場合。
+		} else {
+
+			array[i] = array[i + many];	//任意の要素をi + many個後ろの要素で上書き（数字を前に詰める）。
+
+		}
+	}
 }
