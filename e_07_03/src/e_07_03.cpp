92c79d795c067dedff376d601123a1ee68e64cdb
ddt-asada <ddt.asada@gmail.com>
2017-05-12 13:48:40 +0900

    C++ 演習7-3 修正完了100％ （浅田 テキストの課題に沿うように関数宣言を追加。

--- e_07_03/src/e_07_03.cpp
+++ e_07_03/src/e_07_03.cpp
@@ -2,62 +2,19 @@
  * 値を昇順に並び替える関数をポインタの値渡しによって行うプログラム。
  * 作成日：2017年5月8日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月10日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
 
 using namespace std;
 
-/*関数名：void swap(int* swap1, int* swap2)
- * 渡された引数を入れ替える関数。
- * 引数swap1：入れ替える値。
- * 引数swap2：入れ替える値。
- * 返却値：なし。
- * 作成日：2017年5月8日
- * 作成者：浅田　知嗣
- */
+//関数宣言。
+void swap(int* swap1, int* swap2);
 
-void swap(int* swap1, int* swap2)
-{
-	int	swap3  = *swap1;	//値を入れ替えるための仮の置き場に、swap1を代入。
-		*swap1 = *swap2;	//値を入れ替える。
-		*swap2 = swap3;	//値を入れ替える。
-
-}
-
-/*関数名：void sort(int* sort1, int* sort2, int* sort3)
- * 受け取った引数を昇順に並び替える関数。
- * 引数sort1：並び替えたい変数。
- * 引数sort2：並び替えたい変数。
- * 引数sort3：並び替えたい変数。
- * 返却値：なし。
- * 作成日：2017年5月8日
- * 作成者：浅田　知嗣
- */
-
-void sort(int* sort1, int* sort2, int* sort3)
-{
-	//大小を比較して昇順にソート。
-	if(*sort1 > *sort2) {
-		//値を入れ替える関数に渡す。
-		swap(sort1, sort2);
-
-	}
-
-	//大小を比較して昇順にソート。
-	if(*sort2 > *sort3) {
-		//値を入れ替える関数に渡す。
-		swap(sort2, sort3);
-
-	}
-
-	//大小を比較して昇順にソート。
-	if(*sort1 > *sort2) {
-		//値を入れ替える関数に渡す。
-		swap(sort1, sort2);
-
-	}
-}
+//関数宣言。
+void sort(int* sort1, int* sort2, int* sort3);
 
 int main()
 {
@@ -96,3 +53,59 @@ int main()
 	//0を返す。
 	return 0;
 }
+
+/*関数名：void swap(int* swap1, int* swap2)
+ * 渡された引数を入れ替える関数。
+ * 引数swap1：入れ替える値。
+ * 引数swap2：入れ替える値。
+ * 返却値：なし。
+ * 作成日：2017年5月8日
+ * 作成者：浅田　知嗣
+ * 更新日：2017年5月10日
+ * 更新者：浅田　知嗣
+ */
+
+void swap(int* swap1, int* swap2)
+{
+	int	swap3  = *swap1;	//値を入れ替えるための仮の置き場に、swap1を代入。
+		*swap1 = *swap2;	//値を入れ替える。
+		*swap2 = swap3;	//値を入れ替える。
+
+}
+
+/*関数名：void sort(int* sort1, int* sort2, int* sort3)
+ * 受け取った引数を昇順に並び替える関数。
+ * 引数sort1：並び替えたい変数。
+ * 引数sort2：並び替えたい変数。
+ * 引数sort3：並び替えたい変数。
+ * 返却値：なし。
+ * 作成日：2017年5月8日
+ * 作成者：浅田　知嗣
+ * 更新日：2017年5月10日
+ * 更新者：浅田　知嗣
+ */
+
+void sort(int* sort1, int* sort2, int* sort3)
+{
+	//大小を比較して昇順にソート。
+	if(*sort1 > *sort2) {
+		//値を入れ替える関数に渡す。
+		swap(sort1, sort2);
+
+	}
+
+	//大小を比較して昇順にソート。
+	if(*sort2 > *sort3) {
+		//値を入れ替える関数に渡す。
+		swap(sort2, sort3);
+
+	}
+
+	//大小を比較して昇順にソート。
+	if(*sort1 > *sort2) {
+		//値を入れ替える関数に渡す。
+		swap(sort1, sort2);
+
+	}
+}
+
