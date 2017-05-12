d3fa019c00d7f76e5fcf446eb11f7e2e905de554
ddt-asada <ddt.asada@gmail.com>
2017-05-12 13:52:46 +0900

    C++ 演習7-14 修正完了100％ （浅田 テキストの課題に沿うように関数宣言を追加。

--- e_07_14/src/e_07_14.cpp
+++ e_07_14/src/e_07_14.cpp
@@ -3,51 +3,16 @@
  * 返却するのはkeyと等しい要素の個数。
  * 作成日：2017年5月9日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月10日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
 
 using namespace std;
 
-/*関数名：search_KEY(const int* Array, Key,no)
- * 引数Array：線形探索する配列。
- * 引数key：探索する値。
- * 引数no：配列の要素数。
- * 返却値：配列Arrayに存在するkeyと等しい要素の数。
- * 作成日：2017年5月9日
- * 作成者：浅田　知嗣
- */
-
-int search_KEY(const int* Array, int key, int no)
-{
-	int	KEY[no];	//keyと等しい値が格納されている要素の添字を格納する配列。
-
-	int	Count = 0;	//keyと等しい値が含まれている数を数えるための変数。
-
-	//配列の全要素を線形探索する。
-	for(int i = 0; i < no; i++) {
-
-		//keyと等しい値を見つけたとき。
-		if(Array[i] == key) {
-
-			KEY[Count] = i;		//等しい値が含まれていた場所を覚えさせる。
-
-			Count++;			//見つけた回数をカウント。
-
-		}
-	}
-
-	//配列のどこに同じ値が含まれていたかを表示する。
-	for(int i = 0; i < Count; i++) {
-
-		//配列のどこに同じ値が含まれていたかを表示する。
-		cout	<<key	<<"と同じ値は配列Arrayの"	<<KEY[i]	<<"に含まれていました。\n";
-
-	}
-
-	//見つけた個数を返す。
-	return Count;
-}
+//関数の宣言。
+int search_KEY(const int* Array, int key, int no);
 
 int main()
 {
@@ -77,4 +42,46 @@ int main()
 
 	//0を返す。
 	return 0;
+}
+
+/*関数名：search_KEY(const int* Array, Key,no)
+ * 引数Array：線形探索する配列。
+ * 引数key：探索する値。
+ * 引数no：配列の要素数。
+ * 返却値：配列Arrayに存在するkeyと等しい要素の数。
+ * 作成日：2017年5月9日
+ * 作成者：浅田　知嗣
+ * 更新日：2017年5月10日
+ * 更新者：浅田　知嗣
+ */
+
+int search_KEY(const int* Array, int key, int no)
+{
+	int	KEY[no];	//keyと等しい値が格納されている要素の添字を格納する配列。
+
+	int	Count = 0;	//keyと等しい値が含まれている数を数えるための変数。
+
+	//配列の全要素を線形探索する。
+	for(int i = 0; i < no; i++) {
+
+		//keyと等しい値を見つけたとき。
+		if(Array[i] == key) {
+
+			KEY[Count] = i;		//等しい値が含まれていた場所を覚えさせる。
+
+			Count++;			//見つけた回数をカウント。
+
+		}
+	}
+
+	//配列のどこに同じ値が含まれていたかを表示する。
+	for(int i = 0; i < Count; i++) {
+
+		//配列のどこに同じ値が含まれていたかを表示する。
+		cout	<<key	<<"と同じ値は配列Arrayの"	<<KEY[i]	<<"番目に含まれていました。\n";
+
+	}
+
+	//見つけた個数を返す。
+	return Count;
 }
