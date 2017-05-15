ab54cfa6db8c473c7cc39a1bb1d0787e75e844ca
ddt-asada <ddt.asada@gmail.com>
2017-05-15 18:10:19 +0900

    C++ 演習8-20 修正中90％ （浅田 ポインタのポインタによりアドレスの交換を行うように変更。

--- e_08_20/src/e_08_20.cpp
+++ e_08_20/src/e_08_20.cpp
@@ -1,7 +1,9 @@
 /*演習8-20
  * 二つの文字列を指すポインタを交換する関数。
- * 作成日：2017年月13日
+ * 作成日：2017年5月13日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月15日
+ * 更新者：浅田　知嗣
  */
 
 #include <iostream>
@@ -11,13 +13,13 @@
 using namespace std;
 
 //関数宣言。
-void swap_string(char* swap1, char* swap2);
+void swap_string(const char** swap1, const char** swap2);
 
 int main()
 {
-	char swap1[] = "ABCDEFG";		//入れ替えを行う文字列。
+	const char* swap1 = "ABC";		//入れ替えを行う文字列。
 
-	char swap2[] = "123";			//入れ替えを行う文字列。
+	const char* swap2 = "123";			//入れ替えを行う文字列。
 
 	//文字列のポインタの入れ替え前の状態を確認する。
 	cout	<<"入れ替え前は\n";
@@ -25,12 +27,11 @@ int main()
 	cout	<<"swap2："	<<swap2	<<"\nアドレス："	<<&swap2	<<'\n';
 
 	//文字列のポインタを交換する関数を呼び出す。
-	swap_string(swap1, swap2);
-
+	swap_string(&swap1, &swap2);
 	//ポインタが入れ替えられたことを確認する。
 	cout	<<"入れ替え後は\n";
-	cout	<<"swap1："	<<swap1	<<"\nアドレス："	<<&swap1	<<'\n';
-	cout	<<"swap2："	<<swap2	<<"\nアドレス："	<<&swap2	<<'\n';
+	cout	<<"swap1："	<<swap1	<<"\nアドレス："	<<&*swap1	<<'\n';
+	cout	<<"swap2："	<<swap2	<<"\nアドレス："	<<&*swap2	<<'\n';
 }
 
 /*関数名：swap_string(char* swap1, char* swap2)
@@ -38,17 +39,20 @@ int main()
  * 引数char* swap1：入れ替える文字列。
  * 引数char* swap2：入れ替える文字列。
  * 返却値：なし。
- * 作成日：2017年月13日
+ * 作成日：2017年5月13日
  * 作成者：浅田　知嗣
+ * 更新日：2017年5月15日
+ * 更新者：浅田　知嗣
  */
 
-void swap_string(char* swap1, char* swap2)
+void swap_string(const char** swap1, const char** swap2)
 {
-	char	swap3[] = "AAA";	//文字列を入れ替えるときの仮の置き場。
+	//const char*	swap3;	//文字列を入れ替えるときの仮の置き場。
 
-	strcpy(swap3, swap2);	//文字列を仮の置き場に格納。
-	strcpy(swap2, swap1);	//文字列を入れ替える。
-	strcpy(swap1, swap3);	//文字列を戻す。
+	const char*	swap3 = *swap2;	//文字列を仮の置き場に格納。
+	*swap2 = *swap1;	//文字列を入れ替える。
+	*swap1 = swap3;	//文字列を戻す。
 
 }
+
 
